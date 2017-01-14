fdeffb8673f47049c1ae5c529755afa28ca9a2cc
David Mak <david.18.19.21@gmail.com>
2017-01-14 18:58:05 +0800

    Finalize homework and include WIP test scenario

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,6 @@
 #include <libbase/k60/mcg.h>
 #include <libsc/system.h>
 
-#include <bitset>
-#include <string>
-
 namespace libbase {
     namespace k60 {
         Mcg::Config Mcg::GetMcgConfig() {
@@ -62,16 +59,24 @@ void heartbeatTest() {
 int main() {
     System::Init();
 
-    // initialize leds
-    Led::Config config;
-    config.id = 0;
-    Led led1(config);
-    config.id = 1;
-    Led led2(config);
+    Led::Config ledConfig;
+    ledConfig.is_active_low = true;
+    ledConfig.id = 0;
+    Led led1(ledConfig);
+    ledConfig.id = 1;
+    Led led2(ledConfig);
+    ledConfig.id = 2;
+    Led led3(ledConfig);
+    ledConfig.id = 3;
+    Led led4(ledConfig);
+
+    led1.SetEnable(false);
+    led2.SetEnable(true);
+    led3.SetEnable(false);
+    led4.SetEnable(false);
 
     // initialize camera
     k60::Ov7725::Config cameraConfig;
-    cameraConfig.fps = k60::Ov7725Configurator::Config::Fps::kHigh;
     cameraConfig.id = 0;
     cameraConfig.w = 80; // downscale the width to 80
     cameraConfig.h = 60; // downscale the height to 60
@@ -79,14 +84,11 @@ int main() {
 
     // initialize LCD
     St7735r::Config lcdConfig;
-    lcdConfig.is_bgr = true;
     lcdConfig.fps = 100;
     St7735r lcd(lcdConfig);
 
-    // current execution cycle
-    // this ensures that even if the whole retrieve->display process overruns
-    // (System::Time() % 10 == 0), the next cycle will still initiate
-    volatile uint32_t cycleImg = System::Time()/10;
+    // current execution time
+    Timer::TimerInt timeImg = System::Time();
 
     // start the camera before we do anything
     camera.Start();
@@ -96,29 +98,41 @@ int main() {
 
     while (true) {
         // check if we're still in the same cycle
-        if (cycleImg != System::Time()/10) {
-            // update the cycle
-            cycleImg = System::Time()/10;
+        if (timeImg != System::Time()) {
+            // check if it takes shorter than 9ms to read+copy the buffer
+            if ( ( System::Time() - timeImg ) <= 9) {
+                // update the cycle
+                timeImg = System::Time();
 
-            // lock the buffer and copy it
-            const auto bufferSize = camera.GetBufferSize();
-            const Byte* pBuffer = camera.LockBuffer();
-            Byte bufferArr[bufferSize];
-            for (int i = 0; i < bufferSize; ++i) {
-                bufferArr[i] = pBuffer[i];
+                // lock the buffer and copy it
+                const auto bufferSize = camera.GetBufferSize();
+                const Byte* pBuffer = camera.LockBuffer();
+                Byte bufferArr[bufferSize];
+                for (uint16_t i = 0; i < bufferSize; ++i) {
+                    bufferArr[i] = pBuffer[i];
+                }
+
+                // unlock the buffer now that we have the data
+                camera.UnlockBuffer();
+
+                // rewrite it with new data
+                /*lcd.SetRegion(Lcd::Rect(0, 0, 80, 60));
+                lcd.FillBits(Lcd::kBlack, Lcd::kWhite, bufferArr, camera.GetBufferSize() * 8);*/
+            } else {
+                break;
             }
-
-            // unlock the buffer now that we have the data
-            camera.UnlockBuffer();
-
-            // clear the screen and rewrite it with new data
-            lcd.Clear();
-            lcd.FillBits(Lcd::kWhite, Lcd::kBlack, bufferArr, bufferSize);
         }
     }
 
+    // turn on LED4 when the process cannot keep up
+    led4.SetEnable(true);
+
     // clean up resources in case something goes wrong
     camera.Stop();
+
+    // infinite loop to keep the program from terminating
+    while (1) {
+    }
 
     return 0;
 }
