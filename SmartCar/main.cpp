a8c009b1c0eb91a266cfaa4fa80e6256a79a379f
LeeChunHei <lch19980829@gmail.com>
2017-01-18 15:23:44 +0800

    SmartCar Internal File Added (Added Camera Filter Function

--- SmartCar/main.cpp
+++ SmartCar/main.cpp
@@ -10,10 +10,18 @@
 #include <cstring>
 #include <libbase/k60/mcg.h>
 #include <libsc/system.h>
-#include <libsc/led.h>
+
+//Button Header File
 #include<libsc/button.h>
+
+//led Header File
+#include <libsc/led.h>
+
+//LCD Header File
 #include<libsc/st7735r.h>
 #include<libsc/lcd_console.h>
+
+//Camera Header File
 #include<libsc/k60/ov7725.h>
 
 namespace libbase
@@ -38,6 +46,54 @@ using namespace libsc;
 using namespace libsc::k60;
 using namespace libbase::k60;
 
+const int CamHeight=60;
+const int CamWidth=80;
+
+//Change Camera Buffer from 1D array to 2D array----------------------------------------------------------------------------
+//row is row number
+//col is column number
+//CamByte is the Byte CameraBuffer
+int Cam2DArray[CamWidth][CamHeight];
+void Camera2DConverter(const Byte* CameraBuffer){
+	Byte CamByte;
+	for(Uint row=0;row<CamHeight;row++){
+		for(Uint col=0;col<CamWidth;col+=8){
+			CamByte=CameraBuffer[row*CamWidth/8+col/8];
+			Cam2DArray[row][col]=CamByte&0x80;
+			Cam2DArray[row][col+1]=CamByte&0x40;
+			Cam2DArray[row][col+2]=CamByte&0x20;
+			Cam2DArray[row][col+3]=CamByte&0x10;
+			Cam2DArray[row][col+4]=CamByte&0x08;
+			Cam2DArray[row][col+5]=CamByte&0x04;
+			Cam2DArray[row][col+6]=CamByte&0x02;
+			Cam2DArray[row][col+7]=CamByte&0x01;
+		}
+	}
+}
+
+//image filter
+void CameraFilter(){
+	Uint temp;
+	for(Uint row=1;row<(CamHeight-1);row++){
+		for(Uint col=1;col<(CamWidth-1);col++){
+			temp=temp+Cam2DArray[row-1][col-1]+Cam2DArray[row-1][col]+Cam2DArray[row-1][col+1]
+					 +Cam2DArray[row][col-1]+Cam2DArray[row][col]+Cam2DArray[row][col+1]
+					 +Cam2DArray[row+1][col-1]+Cam2DArray[row+1][col]+Cam2DArray[row+1][col+1];
+			if(temp>4){
+				Cam2DArray[row-1][col-1]=1;
+				Cam2DArray[row-1][col]=1;
+				Cam2DArray[row-1][col+1]=1;
+				Cam2DArray[row][col-1]=1;
+				Cam2DArray[row][col]=1;
+				Cam2DArray[row][col+1]=1;
+			    Cam2DArray[row+1][col-1]=1;
+			    Cam2DArray[row+1][col]=1;
+			    Cam2DArray[row+1][col+1]=1;
+			}
+		}
+	}
+}
+
 int main(void)
 {
 	System::Init();
@@ -48,56 +104,22 @@ int main(void)
 	ConfigLCD.fps=60;
 	St7735r LCD(ConfigLCD);
 
-	/*
-	LcdConsole::Config ConfigConsole;
-	ConfigConsole.lcd=&LCD;
-	ConfigConsole.region=Lcd::Rect(0,0,128,80);
-	LcdConsole Console(ConfigConsole);
-	*/
-
 	Ov7725::Config ConfigCam;
 	ConfigCam.id=0;
-	ConfigCam.h=60;
-	ConfigCam.w=80;
+	ConfigCam.h=CamHeight;
+	ConfigCam.w=CamWidth;
 	Ov7725 Cam(ConfigCam);
-
-	//LCD.SetRegion(Lcd::Rect(0,0,80,60));
-
-	Byte *CamBufferPointer;
-	int CamBuffer;
-	int CamArray[80][60];
 
 	while(1){
 		Cam.Start();
 		if(Cam.IsAvailable()){
-			//LCD.FillBits(0x001F,0xFFFF,Cam.LockBuffer(),Cam.GetBufferSize()*8);
-			CamBufferPointer=(Byte*)(Cam.LockBuffer());
-			CamBuffer=(int)CamBufferPointer;
-			int i=7;
-			int j=0;
-			int k=0;
-			while(CamBuffer>0){
-				if(i>-1){
-					CamArray[i+k][j]=CamBuffer%2;
-					CamBuffer=CamBuffer/2;
-					i=i-1;
-				}else if(k>72){
-					i=7;
-					k=k+8;
-					CamBufferPointer=(Byte*)(CamBufferPointer+1);
-					CamBuffer=(int)CamBufferPointer;
-				}else{
-					j=j+1;
-					k=0;
-					if(j==61){
-						break;
-					}
-				}
-			}
-			for(int y=0;y<61;y++){
-				for(int x=0;x<81;x++){
+			Camera2DConverter(Cam.LockBuffer());
+			CameraFilter();
+			//print the 2D Array on the LCD
+			for(int y=0;y<CamHeight;y++){
+				for(int x=0;x<CamWidth;x++){
 					LCD.SetRegion(Lcd::Rect(x,y,1,1));
-					if(CamArray[x][y]==0){
+					if(Cam2DArray[y][x]==0){
 						LCD.FillColor(0x001F);
 					}else{
 						LCD.FillColor(0xFFFF);
