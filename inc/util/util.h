24b8808cd40409c5c8a0b9bc69f3928d4e550e41
David Mak <david.18.19.21@gmail.com>
2017-02-02 10:44:48 +0800

    Array Element Finder Function: Cleanup

--- inc/util/util.h
+++ inc/util/util.h
@@ -14,6 +14,20 @@ namespace util {
 template<bool B, class T = void>
 using enable_if_t = typename std::enable_if<B, T>::type;
 
+/**
+ * Finds a specified element in an integer-typed array. Searches indices [start, end].
+ *
+ * @note start and end can be flipped to iterate the array backwards.
+ * @note Function does not include bounds checking.
+ *
+ * @tparam T An integer primitive type
+ * @param arr Data array
+ * @param first Starting index
+ * @param last Ending index
+ * @param value The value to find
+ * @param return_last If true, returns last element if value is not found. Otherwise, returns -1.
+ * @return Index of first matching element if found. Otherwise dependent on return_last.
+ */
 template<class T, typename = enable_if_t<std::is_integral<T>::value>>
-int find_element(T *arr, int first, int last, T value);
+int find_element(T *, int, int, T, bool);
 }  // namespace util
--- src/util/util.cpp
+++ src/util/util.cpp
@@ -6,21 +6,8 @@
 #include "../../inc/util/util.h"
 
 namespace util {
-/**
- * Finds a specified element in an integer-typed array. Searches indices [start, end].
- *
- * @note start and end can be flipped to iterate the array backwards.
- * @note Function does not include bounds checking.
- *
- * @tparam T An integer primitive type
- * @param arr Data array
- * @param first Starting index
- * @param last Ending index
- * @param value The value to find
- * @return Index of first matching element if found. -1 otherwise.
- */
 template<class T, typename = enable_if_t<std::is_integral<T>::value>>
-int find_element(T *arr, int first, int last, T value) {
+int find_element(T *arr, int first, int last, T value, bool return_last = false) {
   if (last > first) {
     for (; first <= last; ++first) {
       if (arr[first] == value) {
@@ -34,8 +21,11 @@ int find_element(T *arr, int first, int last, T value) {
       }
     }
   } else if (first == last) {
-    return arr[first] == value ? first : -1;
+    if (arr[first] == value) {
+      return first;
+    }
+    return return_last ? last : -1;
   }
-  return -1;
+  return return_last ? last : -1;
 }
 }  // namespace util
