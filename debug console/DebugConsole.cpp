68cdf898708a2b9b16d17eff3fdabcce1413a8d2
dipsywong98 <facebookarmyoffcal@gmail.com>
2017-02-18 23:08:14 +0800

    Improvements done
    and I forgot what I have improved orz...

--- debug console/DebugConsole.cpp
+++ debug console/DebugConsole.cpp
@@ -31,19 +31,58 @@ using namespace std;
 
 
 //=================================
+#define ON_IDLE 	0
+#define ON_CLICK 	1
+#define ON_LEFT 	2
+#define ON_RIGHT 	3
+#define LONG_IDLE 	4
+#define LONG_CLICK 	5
+#define LONG_LEFT 	6
+#define LONG_RIGHT 	7
 
 class DebugConsole{
 	public:
+		typedef void(*Fptr)();
 		class Item{
 			public:
-			Item(char* n):text(n){}
-			Item(char* n,int* v):text(n),value(v){}
-			Item(){}
-			char* text;
-			void(*onSelectListener)();
-			void(*onLeftClickListener)();
-			void(*onRightClickListener)();
-			int* value=NULL;
+			
+			//Item(char* n):text(n){init();}
+			Item(char* text=NULL,int* value=NULL,bool readOnly=true):text(text),value(value),readOnly(readOnly){init();}
+			//Item(){init();}
+			/*Item(const Item &item){
+				this->setText(item.getText());
+				value=item.getValuePtr();
+				for(int i=0;i<8;i++)
+					listeners[i] = item.getListener(i);
+			}*/
+			
+			//display text methods
+			char* getText(){return text;}
+			void setText(char* t){text=t;}
+			
+			//listener methods
+			Fptr getListener(int type){return listeners[type];}
+			Fptr getListeners(){return *listeners;}
+			void setListener(int type, Fptr fptr){listeners[type]=fptr;}
+			
+			//value methods
+			void setValuePtr(int* v){value=v;}
+			void setValue(int v){*value=v;}
+			int* getValuePtr(){return value;}
+			int getValue(){return *value;}
+			
+			bool isReadOnly(){return readOnly;}
+			void setReadOnly(bool isReadOnly){readOnly=isReadOnly;}
+			
+			private:
+				Fptr listeners[8];
+				int* value=NULL;
+				char* text;
+				bool readOnly;
+				void init(){
+					for(int i=0;i<8;i++)
+						listeners[i]=NULL;
+				}
 		};
 	public:
 		
@@ -59,36 +98,35 @@ class DebugConsole{
 			while(flag){
 				if(kbhit()){
 					char key=getch();
-					if (key=='p'||key==' '){key=getch();}	//P = pause
 					Item item=items[focus];
 					switch(key){
-						case'w':focus=(focus+1)%length;showFocus();break;
-						case's':focus=(focus-1+length)%length;showFocus();break;
+						case's':focus=(focus+1)%length;showFocus();break;
+						case'w':focus=(focus-1+length)%length;showFocus();break;
 						case' ':
-							if(item.onSelectListener!=NULL)
-								item.onSelectListener();
-							else if(item.text=="exit")
+							if(item.getListener(ON_CLICK)!=NULL)
+								item.getListener(ON_CLICK)();
+							else if(item.getText()=="exit")
 								flag=0;
 							printxy(0,10,"enter");
 							break;
 						case'a':
-							if(item.onLeftClickListener!=NULL)
-								item.onLeftClickListener();
-							else if(item.value!=NULL){
-								(*item.value)--;
+							if(item.getListener(ON_LEFT)!=NULL)
+								item.getListener(ON_LEFT)();
+							else if(item.getValuePtr()!=NULL){
+								item.setValue(item.getValue()-1);
 								char buff[20];
-								sprintf(buff,"%s = %d      ",item.text,*item.value);
+								sprintf(buff,"%s = %d      ",item.getText(),item.getValue());
 								printxy(1,focus,buff);
 							}
 							
 							break;
 						case'd':
-							if(item.onLeftClickListener!=NULL)
-								item.onLeftClickListener();
-							else if(item.value!=NULL){
-								(*item.value)++;
+							if(item.getListener(ON_RIGHT)!=NULL)
+								item.getListener(ON_RIGHT)();
+							else if(item.getValuePtr()!=NULL){
+								item.setValue(item.getValue()+1);
 								char buff[20];
-								sprintf(buff,"%s = %d      ",item.text,*item.value);
+								sprintf(buff,"%s = %d      ",item.getText(),item.getValue());
 								printxy(1,focus,buff);
 							}
 							
@@ -100,7 +138,8 @@ class DebugConsole{
 		}
 		
 		void pushItem(Item item){
-			items[length++]=item;
+			insertItem(item,length-1);
+			//items[length++]=item;
 		}
 		
 		void insertItem(Item item, int index=0){
@@ -112,12 +151,12 @@ class DebugConsole{
 		
 		void listItems(){
 			for(int i=0;i<length;i++){
-				if(items[i].value!=NULL){
+				if(items[i].getValuePtr()!=NULL){
 					char buff[20];
-					sprintf(buff,"%s = %d      ",items[i].text,*items[i].value);
+					sprintf(buff,"%s = %d      ",items[i].getText(),items[i].getValue());
 					printxy(1,i,buff);
 				}else
-					printxy(1,i,items[i].text);
+					printxy(1,i,items[i].getText());
 			}
 			showFocus(0);
 		}
@@ -143,6 +182,9 @@ class DebugConsole{
 		}
 };
 
+
+void gt();
+DebugConsole page2;
 int main(){
 	DebugConsole console;
 	
@@ -150,7 +192,16 @@ int main(){
 	
 	console.insertItem(DebugConsole::Item("hi",&x));
 	
+	
+	DebugConsole::Item gtp2("goto page2");
+	
+	gtp2.setListener(ON_CLICK,&gt);
+	console.insertItem(gtp2);
+	
 	console.enterDebug();
 	
 	cout<<"debug end";
+}
+void gt(){
+		page2.enterDebug();
 }
