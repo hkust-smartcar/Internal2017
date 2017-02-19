c475a862eeb2730d0c066fbd7fb1e1b4eb3e187d
dipsywong98 <facebookarmyoffcal@gmail.com>
2017-02-19 12:19:41 +0800

    Update DebugConsole.cpp
    damn I forget what I have upgraded again

--- debug console/DebugConsole.cpp
+++ debug console/DebugConsole.cpp
@@ -30,15 +30,40 @@ using namespace std;
 #endif
 
 
-//=================================
-#define ON_IDLE 	0
-#define ON_CLICK 	1
-#define ON_LEFT 	2
-#define ON_RIGHT 	3
-#define LONG_IDLE 	4
-#define LONG_CLICK 	5
-#define LONG_LEFT 	6
-#define LONG_RIGHT 	7
+//syntax sugars
+//just down or start
+#define START_IDLE 	0
+#define DOWN_SELECT 1
+#define DOWN_LEFT 	2
+#define DOWN_RIGHT 	3
+//just up or end5
+#define END_IDLE 	4
+#define UP_SELECT 	5
+#define UP_LEFT 	6
+#define UP_RIGHT 	7
+//down for a while
+#define LONG_IDLE 	8
+#define LONG_SELECT 9
+#define LONG_LEFT 	10
+#define LONG_RIGHT 	11
+
+#define IDLE 	0
+#define SELECT 	1
+#define LEFT 	2
+#define RIGHT	3
+
+#define START 	0
+#define END 	1
+#define DOWN 	0
+#define UP		1
+#define LONG 	2
+
+
+#define GET_TIME 	clock()
+
+
+//TODO 1 srink in LCD displayable
+//TODO 2 readOnly
 
 class DebugConsole{
 	public:
@@ -75,10 +100,11 @@ class DebugConsole{
 			void setReadOnly(bool isReadOnly){readOnly=isReadOnly;}
 			
 			private:
-				Fptr listeners[8];
+				Fptr listeners[12];
 				int* value=NULL;
 				char* text;
 				bool readOnly;
+				bool upLongExclusive;
 				void init(){
 					for(int i=0;i<8;i++)
 						listeners[i]=NULL;
@@ -87,52 +113,31 @@ class DebugConsole{
 	public:
 		
 		
-		DebugConsole():length(0),focus(0){
+		DebugConsole():length(0),focus(0),topIndex(0){
 			Item item("exit");
 			pushItem(item);
 		}
 		
 		void enterDebug(){
 			listItems();
-			int flag=1;
+			int flag=1,time_next;
+			char key_img;
 			while(flag){
 				if(kbhit()){
 					char key=getch();
 					Item item=items[focus];
-					switch(key){
-						case's':focus=(focus+1)%length;showFocus();break;
-						case'w':focus=(focus-1+length)%length;showFocus();break;
-						case' ':
-							if(item.getListener(ON_CLICK)!=NULL)
-								item.getListener(ON_CLICK)();
-							else if(item.getText()=="exit")
-								flag=0;
-							printxy(0,10,"enter");
-							break;
-						case'a':
-							if(item.getListener(ON_LEFT)!=NULL)
-								item.getListener(ON_LEFT)();
-							else if(item.getValuePtr()!=NULL){
-								item.setValue(item.getValue()-1);
-								char buff[20];
-								sprintf(buff,"%s = %d      ",item.getText(),item.getValue());
-								printxy(1,focus,buff);
-							}
-							
-							break;
-						case'd':
-							if(item.getListener(ON_RIGHT)!=NULL)
-								item.getListener(ON_RIGHT)();
-							else if(item.getValuePtr()!=NULL){
-								item.setValue(item.getValue()+1);
-								char buff[20];
-								sprintf(buff,"%s = %d      ",item.getText(),item.getValue());
-								printxy(1,focus,buff);
-							}
-							
-							break;
+					if (key!=key_img){
+						key_img=key;
+						flag = listen(key,DOWN);
+						time_next=GET_TIME+threshold;
 					}
+					else if(GET_TIME>time_next){
+						flag = listen(key,LONG);
+					}
+					
 				}
+				flag = listen(key_img,UP);
+				key_img='\o';
 				
 			}
 		}
@@ -149,22 +154,29 @@ class DebugConsole{
 			items[index]=item;
 		}
 		
-		void listItems(){
-			for(int i=0;i<length;i++){
-				if(items[i].getValuePtr()!=NULL){
-					char buff[20];
-					sprintf(buff,"%s = %d      ",items[i].getText(),items[i].getValue());
-					printxy(1,i,buff);
-				}else
-					printxy(1,i,items[i].getText());
+		void listItems(int start=0){
+			clear();
+			for(int i=start;i<(length<start+10?length:start+10);i++){
+				printItem(i);
 			}
 			showFocus(0);
+		}
+		
+		void printItem(int index){
+			if(items[index].getValuePtr()!=NULL){
+					char buff[20];
+					sprintf(buff,"%s = %d      ",items[index].getText(),items[index].getValue());
+					printxy(1,index-topIndex,buff);
+				}else
+					printxy(1,index-topIndex,items[index].getText());
 		}
 		
 	private:
 		
 		int length;
 		int focus;
+		int topIndex;
+		int threshold;
 		Item items[1000];
 		
 		
@@ -177,11 +189,61 @@ class DebugConsole{
 		
 		void showFocus(bool flag=1){
 			if(flag) printf(" ");
-			gotoxy(0,focus);
+			gotoxy(0,focus-topIndex);
 			printf(">\b");
 		}
+		
+		void clear(){system("CLS");}
+		
+		bool listen(char key,int state){
+			Item item=items[focus];
+			switch(key){
+				case's':
+					if(state!=UP)
+						focus=(focus+1)%length;
+					break;
+				case'w':
+					if(state!=UP)
+						focus=(focus-1+length)%length;
+					break;
+				case' ':
+					if(item.getListener(SELECT+state*4)!=NULL){
+						item.getListener(SELECT+state*4)();
+						//listItems(topIndex);
+					}
+					else if(item.getText()=="exit")
+						return 0;
+					break;
+				case'a':
+					if(item.getListener(LEFT+state*4)!=NULL){
+						item.getListener(LEFT+state*4)();
+						//listItems(topIndex);
+					}
+					else if(item.getValuePtr()!=NULL&&state!=UP&&!item.isReadOnly()){
+						item.setValue(item.getValue()-1);
+						//printItem(focus);
+					}
+					
+					break;
+				case'd':
+					if(item.getListener(RIGHT+state*4)!=NULL){
+						item.getListener(RIGHT+state*4)();
+						//listItems(topIndex);
+					}
+					else if(item.getValuePtr()!=NULL&&state!=UP&&!item.isReadOnly()){
+						item.setValue(item.getValue()+1);
+						//printItem(focus);
+					}
+					
+					break;
+				default:
+					return 1;
+			}
+			//showFocus();
+			listItems(topIndex);
+			return 1;
+		}
 };
-
 
 void gt();
 DebugConsole page2;
@@ -191,11 +253,13 @@ int main(){
 	int x=0;
 	
 	console.insertItem(DebugConsole::Item("hi",&x));
+	console.insertItem(DebugConsole::Item("nope"));
+	console.insertItem(DebugConsole::Item("xddd"));
 	
 	
 	DebugConsole::Item gtp2("goto page2");
 	
-	gtp2.setListener(ON_CLICK,&gt);
+	gtp2.setListener(DOWN_SELECT,&gt);
 	console.insertItem(gtp2);
 	
 	console.enterDebug();
