56701599edde981c4149bf5fc63ef4af1111c7a4
李婷婷 <tina1998612@users.noreply.github.com>
2017-03-14 13:04:14 +0800

    Update main(for new processing code).cpp

--- main(for new processing code).cpp
+++ main(for new processing code).cpp
@@ -1,11 +1,3 @@
-/*
- * main.cpp
- *
- * Author: Peter
- * Copyright (c) 2014-2015 HKUST SmartCar Team
- * Refer to LICENSE for details
- */
-
 #include <cassert>
 #include <cstring>
 #include <libbase/k60/mcg.h>
