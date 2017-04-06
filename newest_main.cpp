ccf3a17cbe7931eab2400330fdf5c592b816cda5
李婷婷 <tina1998612@users.noreply.github.com>
2017-04-06 17:13:48 +0800

    fix speed display bug

--- newest_main.cpp
+++ newest_main.cpp
@@ -61,12 +61,13 @@ int centerLine = 40, black_img_in_trackL[80], black_img_in_trackR[80];
 double intervalMs = 100;
 int max_servoDeg = 45, min_servoDeg = -45, sideL[80], sideR[80];
 Byte center[80];
-bool inAuto = true;
+Byte inAuto = 1;
 double data_string[20]={};
 //for pid
 double Kp, Ki, I, Kd, output, err, lastInput, Input;
 string var_string;
-int data_string_len, in_turn=0;
+int data_string_len;
+Byte in_turn=0;
 string s = "";
 int k = 0, flag = 0, in_round;
 size_t len = 0;
@@ -140,9 +141,9 @@ void setPidTuning(double kp, double ki, double kd){
 void setAuto(bool toAuto){
 	if(toAuto) {
 		pidInit();
-		inAuto = true;
+		inAuto = 1;
 	} else{
-		inAuto = false;
+		inAuto = 0;
 		stopMotor();
 	}
 }
@@ -162,15 +163,15 @@ void PID(double input, double setPoint){
 
 	lastInput = input;
 }
-void update_data(double* data, int data_len){
-	if(data[8]==0) return;
-	setPidTuning(data[0]/100,data[1]/100,data[2]/100);
-	motorPower = data[3]/100;
-	max_servoDeg = data[4]/100;
-	min_servoDeg = data[5]/100;
-	max_speed = data[6]/100;
-	min_speed = data[7]/100;
-	intervalMs = data[8]/100;
+void update_data(double* data_bt, int data_len){
+	if(data_bt[8]==0) return;
+	setPidTuning(data_bt[0]/100,data_bt[1]/100,data_bt[2]/100);
+	motorPower = data_bt[3]/100;
+	max_servoDeg = data_bt[4]/100;
+	min_servoDeg = data_bt[5]/100;
+	max_speed = data_bt[6]/100;
+	min_speed = data_bt[7]/100;
+	intervalMs = data_bt[8]/100;
 
 	//int hi;
 	//hi = flash->Write(data, 9);
@@ -182,18 +183,18 @@ void sendReceivedChar(Byte data){
 bool BTonReceiveInstruction(const Byte *data, const size_t size){
 	switch(data[0]){
 	case 0:
-		inAuto = true;
+		inAuto = 1;
 		setServoDegree(0);
 		break;
 	case 1:
-		inAuto = false;
+		inAuto = 0;
 		stopMotor();
 		break;
 	case 2:
 		stopMotor();
 		break;
 	case ' ':
-		inAuto = false; //press space key to enter manual mode
+		inAuto = 0; //press space key to enter manual mode
 		stopMotor();
 		setMotorDir(0);
 		break;
@@ -241,7 +242,7 @@ bool BTonReceiveInstruction(const Byte *data, const size_t size){
 	return true;
 }
 void about_centerline(const Byte* camPtr){
-	int area_dif=0, x, white_area = 0, black_img_in_track_time=0;
+	int area_dif=0, x, white_area = 0, black_img_find=0;
 	for(int line=0;line<60;line++){
 		if(white_area>78) {
 			center[line-1] = center[line]; // if cross road -> follows the last center line (so the route won't shift a lot)
@@ -259,6 +260,7 @@ void about_centerline(const Byte* camPtr){
 					if(maybe_black_img_in_track){
 						maybe_black_img_in_track=0;
 						black_img_in_trackR[line] = x-2;
+						black_img_find = 1;
 					}
 					if(!find_sideL){
 						sideL[line] = x-2;
@@ -270,7 +272,7 @@ void about_centerline(const Byte* camPtr){
 					if(!black_img_in_trackL[line]) black_img_in_trackL[line] = x-2;
 					sideR[line] = x-2; // rewrite the value whenever this pattern is detected -> find the right most edge
 				}
-				if(line<5) { // find the area diff on the top region of the img (decide the turning direction in a roundabout)
+				if(!black_img_find) { // find the area diff on the top region of the img (decide the turning direction in a roundabout)
 					if(x>40) area_dif+=image[line][x-1];
 					else area_dif-=image[line][x-1];
 				} else{ // area_dif is done calculated
@@ -281,9 +283,14 @@ void about_centerline(const Byte* camPtr){
 				}
 			}
 		}
+
+		if(white_area<0.2*80) { // if almost no white track detected -> do not count this center line
+			center[line] = 100;
+			continue;
+		}
+
 		if(!image[line][x-1]) sideR[line]=80; // essential because the pattern WWB may exist and rewrite the original value(80) while it is a black img in track instead of sideR
 		if(black_img_in_trackR[line]){ // if black img in track
-			black_img_in_track_time++;
 			// roundabout or obstacles detected
 				if(sideR[line]-sideL[line] > 80*0.75){ // if track edge width > 75% of the horizontal line(TBD) -> roundabout
 					if(in_turn==1){ // turn right
@@ -299,18 +306,26 @@ void about_centerline(const Byte* camPtr){
 					}
 				}
 		}
+
+		if(in_turn && white_area>78){ // come to the end of roundabout
+			if(in_turn==1){ //turn right
+				sideL[line] = 40;
+				sideR[line] = 80;
+			} else{
+				sideR[line] = 40;
+				sideL[line] = 0;
+			}
+		}
+
 		center[line] = (sideL[line]+sideR[line])/2;
 
 		if(line>10){ // cannot smooth the top 10 lines, otherwise array index will be negative
-			if (abs(center[line-10]-center[line-9])>5){ // smooth center line
+			if (center[line-10]<90 && center[line-9]<90 && abs(center[line-10]-center[line-9])>5){ // smooth center line
 				for (int k=0;k<10;k++){
 					center[line-10+k] = (center[line]-center[line-10])*k/9 + center[line-10]; // modify the center line 10 lines closer to our car(so as to turn earlier)
 				}
 			}
 		}
-	}
-	if(black_img_in_track_time<5) { // if return to normal road -> finish turning
-		in_turn = 0;
 	}
 }
 void send_image_BT(const Byte* camPtr, Uint size){
@@ -322,6 +337,9 @@ void tft(const Byte* camPtr, Uint size){
 	screen->FillBits(St7735r::kBlack,St7735r::kWhite,camPtr,8*size);
 
 	for(int line=0;line<60;line++){
+		if(center[line]>80) {
+			continue;
+		}
 
 		screen->SetRegion(Lcd::Rect(center[line], line-1, 2, 2));
 		screen->FillColor(St7735r::kRed);
@@ -334,17 +352,10 @@ void tft(const Byte* camPtr, Uint size){
 
 	}
 }
-void receive_mode_instruction(){
-	Byte isauto = 0;
-	if(inAuto) isauto = 1;
-	else isauto = 0;
-	bluetooth->SendBuffer(temp4,1);
-	bluetooth->SendBuffer(&isauto, 1);
-}
 void send_data_to_bt(int32_t Lencoder, int32_t Rencoder){
-	Byte test[11] = {Kp, Ki/intervalMs, Kd*intervalMs, motorPower, max_servoDeg, min_servoDeg, max_speed, min_speed, intervalMs, Lencoder, Rencoder};
+	Byte test[13] = {Kp*100, Ki/intervalMs*100, Kd*intervalMs*100, motorPower/2, max_servoDeg, min_servoDeg, max_speed, min_speed, intervalMs, Lencoder, Rencoder, in_turn, inAuto};
 	bluetooth->SendBuffer(temp2,1);
-	bluetooth->SendBuffer(test, 11);
+	bluetooth->SendBuffer(test, 13);
 	bluetooth->SendBuffer(center, 60);
 }
 void send_sys_time_to_BT(Byte time){
@@ -412,6 +423,7 @@ int main(void)
 
 	pidInit();
 	//m_flash.Read(data, 9);
+
 	update_data(data, 9);
 
 	cam.Start();
@@ -428,7 +440,6 @@ int main(void)
 				tft(camPtr, camSize);
 
 				PID(center[50], car_center); // use 50 for now
-				//receive_mode_instruction();
 				if(inAuto) startMotor();
 				send_image_BT(camPtr, camSize);
 
