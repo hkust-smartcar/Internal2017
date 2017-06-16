374c2b94e91d1ecc0cbf135f2ccc05dc5e6102bf
dipsywong98 <dipsywong98@users.noreply.github.com>
2017-06-16 22:44:17 +0800

    Added debug console v3
    allow flash, relatively stable

---
+++ debug_console.cpp
@@ -0,0 +1,244 @@
+/*
+ * debug_console.cpp
+ *
+ * Copyright (c) 2014-2017 HKUST SmartCar Team
+ * Refer to LICENSE for details
+ *
+ * Author: Dipsy Wong (dipsywong98)
+ *
+ * Implementation for DebugConsole (v3) class.
+ *
+ */
+
+#include "debug_console.h"
+
+#include <cstdio>
+
+#include "libsc/joystick.h"
+#include "libsc/lcd_typewriter.h"
+#include "libsc/st7735r.h"
+#include "libsc/system.h"
+#include "libbase/k60/flash.h"
+
+using libsc::Joystick;
+using libsc::Lcd;
+using libsc::LcdTypewriter;
+using libsc::St7735r;
+using libsc::System;
+
+Item* Item::SetText(char* t) {
+  text = t;
+  return this;
+}
+
+Item* Item::SetListener(Fptr fptr) {
+  listener = fptr;
+  return this;
+}
+
+Item* Item::SetValuePtr(float* v) {
+  value = v;
+  return this;
+}
+
+Item* Item::SetValue(float v) {
+  if (value != nullptr) *value = v;
+  return this;
+}
+
+Item* Item::SetInterval(float v) {
+  interval = v;
+  return this;
+}
+
+Item* Item::SetReadOnly(bool isReadOnly) {
+  readOnly = isReadOnly;
+  return this;
+}
+
+DebugConsole::DebugConsole(Joystick* joystick, St7735r* lcd, LcdTypewriter* writer, int displayLength)
+    : joystick(joystick), lcd(lcd), writer(writer), displayLength(displayLength) {}
+
+void DebugConsole::EnterDebug() {
+  int flag = 1;
+  Item item(">>exit<<");
+  PushItem(item);
+  int index = items.size();
+  ListItems();
+  while (flag) {
+    Listen();
+  }
+  items.erase(items.end() - (index - items.size()));
+  Clear();
+}
+
+void DebugConsole::Listen() {
+  if (jState != joystick->GetState()) {
+    jState = joystick->GetState();
+    tFlag = System::Time() + threshold;
+    ListenerDo(jState);
+  } else if (System::Time() > tFlag) {
+    tFlag = System::Time() + cd;
+    ListenerDo(jState);
+  }
+}
+
+void DebugConsole::ListItems() {
+  Clear();
+  for (int i = topIndex; i < (items.size() < topIndex + displayLength ? items.size() : topIndex + displayLength); i++) {
+    PrintItem(i, i == focus);
+  }
+}
+
+void DebugConsole::ListItemValues() {
+  Clear();
+  for (int i = topIndex; i < (items.size() < topIndex + displayLength ? items.size() : topIndex + displayLength); i++) {
+    PrintItemValue(i, i == focus);
+  }
+}
+
+void DebugConsole::PrintItem(int index, bool isInverted) {
+  Printxy(0, index - topIndex, items[index].GetText(), isInverted);
+  PrintItemValue(index, isInverted);
+}
+
+void DebugConsole::PrintItemValue(int index, bool isInverted) {
+  if (items[index].GetValuePtr() != nullptr) {
+    char buff[20];
+    sprintf(buff, "%.5lf", items[index].GetValue());
+    Printxy(7, index - topIndex, buff, isInverted);
+  }
+}
+
+void DebugConsole::Load(){
+
+	if(flash == nullptr) return;
+	int start=0;
+	Byte* buff = new Byte[flash_sum*4];
+	flash->Read(buff,flash_sum*4);
+	for(int i=0;i<items.size();i++){
+		if(!items[i].IsFlashable()) continue;
+		float* v=items[i].GetValuePtr();
+		if(v==nullptr)continue;
+		memcpy((unsigned char*) v, buff+start, 4);
+		start+=4;
+		if(*v!=*v)*v=0;
+	}
+	delete [] buff;
+}
+
+void DebugConsole::Save(){
+	if(flash == nullptr) return;
+	int start=0;
+	Byte* buff = new Byte[flash_sum*4];
+	for(int i=0; i<items.size();i++){
+		if(!items[i].IsFlashable()) continue;
+		float* v = items[i].GetValuePtr();
+		if(v==nullptr)continue;
+		memcpy(buff + start, (unsigned char*) v, 4);
+		start+=4;
+	}
+	flash->Write(buff, flash_sum*4);
+	System::DelayMs(100);
+	delete [] buff;
+}
+
+DebugConsole* DebugConsole::SetDisplayLength(int length) {
+  displayLength = length;
+  return this;
+}
+
+DebugConsole* DebugConsole::SetLongClickThreshold(int threshold) {
+  this->threshold = threshold;
+  return this;
+}
+
+DebugConsole* DebugConsole::SetLongClickCd(int cd) {
+  this->cd = cd;
+  return this;
+}
+
+DebugConsole* DebugConsole::SetOffset(int offset) {
+  this->offset = offset;
+  return this;
+}
+
+DebugConsole* DebugConsole::SetAutoFlash(bool flag) {
+  this->auto_flash = flag;
+  return this;
+}
+
+void DebugConsole::Printxy(int x, int y, char* c, int inverted) {
+  if (inverted) {
+    writer->SetTextColor(0x0000);
+    writer->SetBgColor(0xFFFF);
+  }
+  lcd->SetRegion(Lcd::Rect(5 + x * 10, offset + y * 15, 128 - 5 - x * 10, 15));
+  writer->WriteString(c);
+  if (inverted) {
+    writer->SetTextColor(0xFFFF);
+    writer->SetBgColor(0x0000);
+  }
+}
+
+void DebugConsole::Clear() {
+  lcd->SetRegion(Lcd::Rect(0, offset, 128, displayLength * 15));
+  lcd->FillColor(0x0000);
+}
+
+int DebugConsole::ListenerDo(Joystick::State key) {
+  Item item = items[focus];
+  switch (key) {
+    case Joystick::State::kDown:
+      PrintItem(focus);    //remove highLighting
+      focus++;                //move down focus
+      if (focus >= items.size()) {    //focus below last item then jump back to first
+        topIndex = 0;
+        focus = 0;
+        ListItems();
+      } else if (focus >= topIndex + displayLength) {    //next page
+        topIndex += displayLength;
+        ListItems();
+      } else {
+        PrintItem(focus, true);    //update highlighted item
+      }
+      break;
+    case Joystick::State::kUp:
+      PrintItem(focus);
+      focus--;
+      if (focus < 0) {
+        focus = items.size() - 1;
+        topIndex = items.size() - displayLength;
+        topIndex = (topIndex > 0 ? topIndex : 0);
+        ListItems();
+      } else if (focus < topIndex) {
+        topIndex -= displayLength;
+        topIndex = (topIndex > 0 ? topIndex : 0);
+        ListItems();
+      } else {
+        PrintItem(focus, true);
+      }
+      break;
+    case Joystick::State::kSelect:
+      if (item.GetListener() != nullptr) {
+        item.GetListener()();
+      } else if (item.GetText() == ">>exit debug<<")
+        return 0;
+      break;
+    case Joystick::State::kLeft:
+      if (item.GetValuePtr() != nullptr && !item.IsReadOnly()) {
+        item.SetValue(item.GetValue() - item.GetInterval());
+        PrintItemValue(focus, true);
+      }
+      break;
+    case Joystick::State::kRight:
+      if (item.GetValuePtr() != nullptr && !item.IsReadOnly()) {
+        item.SetValue(item.GetValue() + item.GetInterval());
+        PrintItemValue(focus, true);
+      }
+      break;
+    default:
+      return 1;
+  }
+  return 1;
+}
---
+++ debug_console.h
@@ -0,0 +1,160 @@
+/*
+ * debug_console.h
+ *
+ * Copyright (c) 2014-2017 HKUST SmartCar Team
+ * Refer to LICENSE for details
+ *
+ * Author: Dipsy Wong (dipsywong98)
+ *
+ * DebugConsole (v3) class
+ * Console-based GUI for debugging purposes.
+ *
+ */
+
+#ifndef CHASING17_DEBUG_CONSOLE_H_
+#define CHASING17_DEBUG_CONSOLE_H_
+
+#include <vector>
+
+#include "libsc/joystick.h"
+#include "libsc/lcd_typewriter.h"
+#include "libsc/st7735r.h"
+#include "libbase/k60/flash.h"
+
+typedef void(* Fptr)();
+
+class Item {
+ public:
+  Item(char* text = nullptr, float* value = nullptr, bool flashable = false,bool readOnly = false)
+      : text(text), value(value), flash(flashable),readOnly(readOnly) {}
+
+  //display text methods
+  char* GetText() { return text; }
+  Item* SetText(char* t);
+
+  //listener methods
+  Fptr GetListenerptr() { return listener; }
+  Fptr GetListener() { return *listener; }
+  Item* SetListener(Fptr fptr);
+
+  //set parameters
+  Item* SetValuePtr(float* v);
+  Item* SetValue(float v);
+  Item* SetInterval(float v);
+  Item* SetReadOnly(bool isReadOnly);
+  Item* SetFlashable(bool flag){flash=flag;return this;}
+
+  //get parameters
+  float ValueIncre() { return *value += interval; }
+  float ValueDecre() { return *value -= interval; }
+  float* GetValuePtr() { return value; }
+  float GetValue() { return value != nullptr ? *value : 0; }
+  float GetInterval() { return interval; }
+  bool IsFlashable(){return flash;}
+  bool IsReadOnly() { return readOnly; }
+
+ private:
+  char* text;
+  float* value = nullptr;
+  bool readOnly;
+  bool flash = false;
+  Fptr listener = nullptr;
+  float interval = 1;
+  void Init() {}
+};
+
+class DebugConsole {
+ public:
+  /**
+   * constructor of debug console
+   * joystick, lcd, writer pointer and the display length, which limit the display size of debug console
+   */
+  DebugConsole(libsc::Joystick* joystick, libsc::St7735r* lcd, libsc::LcdTypewriter* writer, int displayLength = 10);
+
+  /**
+   * pause and start debugging
+   */
+  void EnterDebug();
+
+  /**
+   * just listen and do, no pause
+   */
+  void Listen();
+
+  /**
+   * adding items to debug console
+   */
+  void PushItem(Item item) { items.push_back(item); if(item.IsFlashable())flash_sum++;}
+  void InsertItem(Item item, int index = 0) { items.insert(items.begin() + index, item); if(item.IsFlashable())flash_sum++;}
+
+  /**
+   * print item start from topIndex, total amount displayLength
+   */
+  void ListItems();
+
+  /**
+   * print item start from topIndex, total amount displayLength
+   */
+  void ListItemValues();
+
+  /**
+   * print a single item's text and value
+   */
+  void PrintItem(int index, bool isInverted = false);
+
+  /**
+   * print a single item's value
+   */
+  void PrintItemValue(int index, bool isInverted = false);
+
+  /**
+   * Load all variable of items which were set flashable
+   */
+  void Load();
+
+  /*
+   * Save all variable of items which were set flashable
+   */
+  void Save();
+
+  //parameters setters
+  DebugConsole* SetDisplayLength(int length);
+  DebugConsole* SetLongClickThreshold(int threshold);
+  DebugConsole* SetLongClickCd(int cd);
+  DebugConsole* SetOffset(int offset);
+  DebugConsole* SetAutoFlash(bool flag);
+  void SetFlash(libbase::k60::Flash* flash){this->flash=flash;}
+
+  //parameters getters
+  int GetDisplayLength() { return displayLength; }
+  int GetLongClickThershold() { return threshold; }
+  int GetLongClickCd() { return cd; }
+  int GetOffset() { return offset; }
+  bool IsAutoFlash(){return auto_flash;}
+
+ private:
+  int focus = 0;
+  int topIndex = 0;
+ public:std::vector<Item> items;
+ private:
+  libsc::Joystick* joystick;
+  libsc::St7735r* lcd;
+  libsc::LcdTypewriter* writer;
+  libbase::k60::Flash* flash = nullptr;
+  libsc::Joystick::State jState = libsc::Joystick::State::kIdle;
+  int tFlag = 0;
+  int threshold = 1000; //long click thershold
+  int displayLength;    //limit the region to display
+  int cd = 0;    //time needed to trigger next long click listener
+  int offset = 0; //distance away from top of lcd
+  bool auto_flash = true; //flash
+  int flash_sum=0;
+
+  void Printxy(int x, int y, char* c, int inverted = false);
+
+  void Clear();
+
+  int ListenerDo(libsc::Joystick::State key);
+};
+
+#endif // CHASING17_DEBUG_CONSOLE_H_
